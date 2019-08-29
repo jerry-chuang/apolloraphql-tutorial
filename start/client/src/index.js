3978604e28e1d65244b6203c0d3aad084a904305
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2019-08-28 21:49:13 -0700

    Store and query local data in apollo cache

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -3,23 +3,33 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import gql from "graphql-tag";
 
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
+import Login from './pages/login';
+import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
+
 
 const cache = new InMemoryCache();
+
+
+
 
 const client = new ApolloClient({
   cache,
   link: new HttpLink({
     uri: 'http://localhost:4000/graphql',
-
     headers: {
       authorization: localStorage.getItem('token'),
     },
   }),
+
+  typeDefs,
+  resolvers,
 });
+
 
 cache.writeData({
   data: {
@@ -28,8 +38,21 @@ cache.writeData({
   },
 });
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
-  </ApolloProvider>, document.getElementById('root')
+    <IsLoggedIn />
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
--- start/client/src/pages/launch.js
+++ start/client/src/pages/launch.js
@@ -10,17 +10,17 @@ import { LAUNCH_TILE_DATA } from './launches';
 export const GET_LAUNCH_DETAILS = gql`
   query LaunchDetails($launchId: ID!) {
     launch(id: $launchId) {
+
+      isInCart @client
       site
       rocket {
         type
       }
-
       ...LaunchTile
     }
   }
-
-  
-${LAUNCH_TILE_DATA}`;
+  ${LAUNCH_TILE_DATA}
+`;
 
 export default function Launch({ launchId }) {
   const { data, loading, error } = useQuery(
